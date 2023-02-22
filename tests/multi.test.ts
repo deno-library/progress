98c723717b5a377ab29f27424f14ce7150a9c251
张复星 <zhangfuxing@cmict.chinamobile.com>
2023-02-22 10:13:46 +0800

    test: add multi.test.ts

--- tests/multi.test.ts
+++ tests/multi.test.ts
@@ -1,5 +1,35 @@
-Deno.test(`Use MultiProgressBar in a deno test`, () => {
-  console.info(
-    `Don't run this test with 'deno test', but as 'deno run examples/multi2.ts'`,
-  );
+import { MultiProgressBar } from "../mod.ts";
+import { delay } from "https://deno.land/std@0.177.0/async/delay.ts";
+
+Deno.test(`Use MultiProgressBar in a deno test`, async () => {
+  const title = "download files";
+  const total = 100;
+
+  const bars = new MultiProgressBar({
+    title,
+    // clear: true,
+    complete: "=",
+    incomplete: "-",
+    display: "[:bar] :text :eta",
+  });
+
+  let completed1 = 0;
+  let completed2 = 0;
+
+  while (completed1 <= total || completed2 <= total) {
+    completed1 += 1;
+    completed2 += 2;
+    bars.render([
+      {
+        completed: completed1,
+        total,
+        text: "file1",
+        complete: "*",
+        incomplete: ".",
+      },
+      { completed: completed2, total, text: "file2" },
+    ]);
+
+    await delay(100);
+  }
 });
