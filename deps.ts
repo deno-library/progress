e3beb995663ab3dc95d34d445ad53c9baa10fa0e
zhangfuxing <zhangfuxing@cmict.chinamobile.com>
2024-10-30 17:04:22 +0800

    fix: resolved publishing failure due to missing version constraint

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { bgGreen, bgWhite, stripAnsiCode } from "jsr:@std/fmt/colors";
-export { writeAll } from "jsr:@std/io/write-all";
-// export type { Writer } from "jsr:@std/io/types";
+export { bgGreen, bgWhite, stripAnsiCode } from "jsr:@std/fmt@1.0.3/colors";
+export { writeAll } from "jsr:@std/io@0.225.0/write-all";
+// export type { Writer } from "jsr:@std/io@0.225.0/types";
